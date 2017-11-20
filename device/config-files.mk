89e5121bcbb5b95d156adcac6649cebac999e521
Andreas Schneider <asn@cryptomilk.org>
2017-11-20 14:50:55 +0100

    config-files: Install camera configs

--- device/config-files.mk
+++ device/config-files.mk
@@ -39,3 +39,53 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/config/audio-calibration/Headset_cal.acdb:vendor/etc/acdbdata/Headset_cal.acdb \
     $(DEVICE_PATH)/config/audio-calibration/Speaker_cal.acdb:vendor/etc/acdbdata/Speaker_cal.acdb
 endif
+
+### CAMERA
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/config/camera/camera_config.xml:system/etc/camera/camera_config.xml \
+    $(DEVICE_PATH)/config/camera/csidtg_camera.xml:system/etc/camera/csidtg_camera.xml \
+    $(DEVICE_PATH)/config/camera/csidtg_chromatix.xml:system/etc/camera/csidtg_chromatix.xml \
+    $(DEVICE_PATH)/config/camera/imx214_chromatix.xml:system/etc/camera/imx214_chromatix.xml \
+    $(DEVICE_PATH)/config/camera/imx230_chromatix.xml:system/etc/camera/imx230_chromatix.xml \
+    $(DEVICE_PATH)/config/camera/imx230_qc2002_chromatix.xml:system/etc/camera/imx230_qc2002_chromatix.xml \
+    $(DEVICE_PATH)/config/camera/imx230_qc2002_with_gyro_chromatix.xml:system/etc/camera/imx230_qc2002_with_gyro_chromatix.xml \
+    $(DEVICE_PATH)/config/camera/imx258_bear_chromatix.xml:system/etc/camera/imx258_bear_chromatix.xml \
+    $(DEVICE_PATH)/config/camera/imx258_chromatix.xml:system/etc/camera/imx258_chromatix.xml \
+    $(DEVICE_PATH)/config/camera/imx258_lc898217xc_chromatix.xml:system/etc/camera/imx258_lc898217xc_chromatix.xml \
+    $(DEVICE_PATH)/config/camera/imx258_mono_bear_chromatix.xml:system/etc/camera/imx258_mono_bear_chromatix.xml \
+    $(DEVICE_PATH)/config/camera/imx258_mono_chromatix.xml:system/etc/camera/imx258_mono_chromatix.xml \
+    $(DEVICE_PATH)/config/camera/imx298_chromatix.xml:system/etc/camera/imx298_chromatix.xml \
+    $(DEVICE_PATH)/config/camera/imx298_gt24c64_chromatix.xml:system/etc/camera/imx298_gt24c64_chromatix.xml \
+    $(DEVICE_PATH)/config/camera/imx318_chromatix.xml:system/etc/camera/imx318_chromatix.xml \
+    $(DEVICE_PATH)/config/camera/imx362_chromatix.xml:system/etc/camera/imx362_chromatix.xml \
+    $(DEVICE_PATH)/config/camera/imx362_chromatix_bear.xml:system/etc/camera/imx362_chromatix_bear.xml \
+    $(DEVICE_PATH)/config/camera/imx362_gt24c64a_chromatix.xml:system/etc/camera/imx362_gt24c64a_chromatix.xml \
+    $(DEVICE_PATH)/config/camera/imx376_chromatix.xml:system/etc/camera/imx376_chromatix.xml \
+    $(DEVICE_PATH)/config/camera/imx378_chromatix.xml:system/etc/camera/imx378_chromatix.xml \
+    $(DEVICE_PATH)/config/camera/ov12a10_chromatix.xml:system/etc/camera/ov12a10_chromatix.xml \
+    $(DEVICE_PATH)/config/camera/ov13850_chromatix.xml:system/etc/camera/ov13850_chromatix.xml \
+    $(DEVICE_PATH)/config/camera/ov13850_q13v06k_chromatix.xml:system/etc/camera/ov13850_q13v06k_chromatix.xml \
+    $(DEVICE_PATH)/config/camera/ov13855_chromatix.xml:system/etc/camera/ov13855_chromatix.xml \
+    $(DEVICE_PATH)/config/camera/ov13880_chromatix.xml:system/etc/camera/ov13880_chromatix.xml \
+    $(DEVICE_PATH)/config/camera/ov2281_chromatix.xml:system/etc/camera/ov2281_chromatix.xml \
+    $(DEVICE_PATH)/config/camera/ov2680_chromatix.xml:system/etc/camera/ov2680_chromatix.xml \
+    $(DEVICE_PATH)/config/camera/ov4688_chromatix.xml:system/etc/camera/ov4688_chromatix.xml \
+    $(DEVICE_PATH)/config/camera/ov5670_chromatix.xml:system/etc/camera/ov5670_chromatix.xml \
+    $(DEVICE_PATH)/config/camera/ov5670_f5670bq_chromatix.xml:system/etc/camera/ov5670_f5670bq_chromatix.xml \
+    $(DEVICE_PATH)/config/camera/ov5695_chromatix.xml:system/etc/camera/ov5695_chromatix.xml \
+    $(DEVICE_PATH)/config/camera/ov7251_chromatix.xml:system/etc/camera/ov7251_chromatix.xml \
+    $(DEVICE_PATH)/config/camera/ov8856_chromatix.xml:system/etc/camera/ov8856_chromatix.xml \
+    $(DEVICE_PATH)/config/camera/ov8858_chromatix.xml:system/etc/camera/ov8858_chromatix.xml \
+    $(DEVICE_PATH)/config/camera/ov8865_chromatix.xml:system/etc/camera/ov8865_chromatix.xml \
+    $(DEVICE_PATH)/config/camera/s5k2l7_8953_chromatix.xml:system/etc/camera/s5k2l7_8953_chromatix.xml \
+    $(DEVICE_PATH)/config/camera/s5k2l7_chromatix.xml:system/etc/camera/s5k2l7_chromatix.xml \
+    $(DEVICE_PATH)/config/camera/s5k2l7sx_chromatix.xml:system/etc/camera/s5k2l7sx_chromatix.xml \
+    $(DEVICE_PATH)/config/camera/s5k3l8_chromatix.xml:system/etc/camera/s5k3l8_chromatix.xml \
+    $(DEVICE_PATH)/config/camera/s5k3l8_f3l8yam_chromatix.xml:system/etc/camera/s5k3l8_f3l8yam_chromatix.xml \
+    $(DEVICE_PATH)/config/camera/s5k3l8_mono_chromatix.xml:system/etc/camera/s5k3l8_mono_chromatix.xml \
+    $(DEVICE_PATH)/config/camera/s5k3m2xm_chromatix.xml:system/etc/camera/s5k3m2xm_chromatix.xml \
+    $(DEVICE_PATH)/config/camera/s5k3m2xm_chromatix_bear.xml:system/etc/camera/s5k3m2xm_chromatix_bear.xml \
+    $(DEVICE_PATH)/config/camera/s5k3m2xx_chromatix.xml:system/etc/camera/s5k3m2xx_chromatix.xml \
+    $(DEVICE_PATH)/config/camera/s5k3m3sm_chromatix.xml:system/etc/camera/s5k3m3sm_chromatix.xml \
+    $(DEVICE_PATH)/config/camera/s5k3p3sm_chromatix.xml:system/etc/camera/s5k3p3sm_chromatix.xml \
+    $(DEVICE_PATH)/config/camera/s5k3p8sp_chromatix.xml:system/etc/camera/s5k3p8sp_chromatix.xml
