0e1970d1be1860bb098932d6d11891860dc63c0d
Andreas Schneider <asn@cryptomilk.org>
2018-01-04 08:37:02 +0100

    device: Optionally integrate FDroid and MicroG

--- device.mk
+++ device.mk
@@ -3,6 +3,10 @@ $(call inherit-product, device/sony/yoshino/platform.mk)
 ### PROPRIETARY VENDOR FILES
 $(call inherit-product, vendor/sony/lilac/lilac-vendor.mk)
 
+ifeq ($(WITH_MICROG),true)
+$(call inherit-product, vendor/microg/microg-vendor.mk)
+endif
+
 ### DALVIK/HWUI
 $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
 $(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
