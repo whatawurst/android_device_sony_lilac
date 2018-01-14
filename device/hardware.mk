8e75179729658f1556ae0198d317b0169718315e
Andreas Schneider <asn@cryptomilk.org>
2018-01-14 14:45:23 +0100

    hardware: Build NFC HAL

--- device/hardware.mk
+++ device/hardware.mk
@@ -1,19 +1,8 @@
-ifeq ($(WITH_VENDOR_IMAGE),true)
-
-### LIGHTS
-PRODUCT_PACKAGES += \
-    lights.lilac
-
-### POWER
-PRODUCT_PACKAGES += \
-    power.lilac
-
 ### NFC
 PRODUCT_PACKAGES += \
     nfc_nci.lilac
 
-endif
-
+### HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     android.hidl.manager@1.0
