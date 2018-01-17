239fe36e9b9716641dfda0e029435bde63ef0cf8
Andreas Schneider <asn@cryptomilk.org>
2018-01-17 14:46:53 +0100

    config-files: Update nfc configs and fix install location

--- device/config-files.mk
+++ device/config-files.mk
@@ -1,4 +1,5 @@
 ### NFC
 PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/config/nfc/libnfc-brcm.conf:system/etc/libnfc-brcm.conf \
-    $(DEVICE_PATH)/config/nfc/libnfc-nxp.conf:system/etc/libnfc-nxp.conf
+    $(DEVICE_PATH)/config/nfc/libnfc-brcm.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-brcm.conf \
+    $(DEVICE_PATH)/config/nfc/libnfc-nxp-octopus.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp-octopus.conf \
+    $(DEVICE_PATH)/config/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
