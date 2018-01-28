e2f9c8b23b96bc1f5add67a06dae2eb53340a772
Andreas Schneider <asn@cryptomilk.org>
2018-01-28 13:27:11 +0100

    dependencies: Move hidl deps to own file

---
+++ device/dependencies.mk
@@ -0,0 +1,5 @@
+### HIDL
+PRODUCT_PACKAGES += \
+    android.hidl.base@1.0 \
+    android.hidl.manager@1.0 \
+    android.hidl.manager-V1.0-java
--- device/hardware.mk
+++ device/hardware.mk
@@ -1,8 +1,3 @@
 ### NFC
 PRODUCT_PACKAGES += \
     nfc_nci.msm8998
-
-### HIDL
-PRODUCT_PACKAGES += \
-    android.hidl.base@1.0 \
-    android.hidl.manager@1.0
