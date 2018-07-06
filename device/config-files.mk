c4a8569c492b343a0e22b31dd51d0821c9b75b47
Christian Oder <myself5@carbonrom.org>
2018-07-06 22:33:59 +0200

    lilac: Import audio configs from yoshino tree
    Change-Id: Idb2dac86bde911b1cd775d03a107645c10ce872e

--- device/config-files.mk
+++ device/config-files.mk
@@ -3,3 +3,8 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/config/nfc/libnfc-brcm.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-brcm.conf \
     $(DEVICE_PATH)/config/nfc/libnfc-nxp-octopus.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp-octopus.conf \
     $(DEVICE_PATH)/config/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
+
+### Audio
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/config/audio/mixer_paths_tasha.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_tasha.xml \
+    $(DEVICE_PATH)/config/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml
