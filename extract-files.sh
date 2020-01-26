a86388f9e38a643ca9b63863f44a7f8adcb54d86
derfelot <5124143+derfelot@users.noreply.github.com>
2020-01-26 19:18:52 +0100

    proprietary: Pin blobs that referenced set_sched_policy
    insead of using patchelf on the fly. This removes the requirement to have patchelf installed on build machine.

--- extract-files.sh
+++ extract-files.sh
@@ -34,26 +34,6 @@ if [ ! -f "$HELPER" ]; then
 fi
 . "$HELPER"
 
-function blob_fixup() {
-    case "${1}" in
-    lib/libwfdcommonutils.so)
-        patchelf --replace-needed "libcutils.so" "libprocessgroup.so" "${2}"
-        ;;
-    vendor/lib/libgps.utils.so)
-        patchelf --replace-needed "libcutils.so" "libprocessgroup.so" "${2}"
-        ;;
-    vendor/lib/hw/audio.primary.msm8998.so)
-        patchelf --replace-needed "libcutils.so" "libprocessgroup.so" "${2}"
-        ;;
-    vendor/lib64/libgps.utils.so)
-        patchelf --replace-needed "libcutils.so" "libprocessgroup.so" "${2}"
-        ;;
-    vendor/lib64/hw/audio.primary.msm8998.so)
-        patchelf --replace-needed "libcutils.so" "libprocessgroup.so" "${2}"
-        ;;
-    esac
-}
-
 # Default to sanitizing the vendor folder before extraction
 CLEAN_VENDOR=true
 
