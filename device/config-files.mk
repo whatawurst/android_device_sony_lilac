0a3171a6b3598b3fde34830cd6e0480203b2f9ef
derfelot <derfelot.git@gmail.com>
2021-01-31 02:44:27 +0100

    lilac: Pull unmodified audio and nfc configs from stock
    No need to keep local copies if we don't modify them.

--- device/config-files.mk
+++ device/config-files.mk
@@ -1,8 +1,3 @@
-### Audio
-PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/config/audio/mixer_paths_tasha.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_tasha.xml \
-    $(DEVICE_PATH)/config/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml
-
 ### Modem Configs
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/config/modem/modem.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/customization/modem/modem.conf \
