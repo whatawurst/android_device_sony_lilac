87cd6c67818b9db6fff3c6256dac25b5cc4aa7c5
Andrea <linckandrea01@gmail.com>
2021-05-30 14:35:16 +0200

    lilac: kang powerhint.json from yoshino-common
     * as 67885f7819248801c599a1e42069f7a4a6420cab

--- device.mk
+++ device.mk
@@ -28,4 +28,8 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
     $(DEVICE_PATH)/overlay/packages/apps/CarrierConfig
 
+### POWER
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/config/power/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+
 include $(DEVICE_PATH)/device/*.mk
