920e87e6d8a212246d22ec51c3cde15d984d9b6d
derfelot <derfelot.git@gmail.com>
2021-09-16 02:58:43 +0200

    lilac: Add soong namespace

--- device.mk
+++ device.mk
@@ -18,6 +18,10 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 
 DEVICE_PATH := device/sony/lilac
 
+# Soong
+PRODUCT_SOONG_NAMESPACES += \
+    $(DEVICE_PATH)
+
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal hdpi xhdpi
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
