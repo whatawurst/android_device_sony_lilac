490fffa271df4dd8dd134f6d540fcd0a6680c5bd
derfelot <derfelot.git@gmail.com>
2022-04-16 03:35:18 +0200

    lilac: device: Move common apex and dalvik flags to yoshino-common

--- device.mk
+++ device.mk
@@ -3,21 +3,12 @@ $(call inherit-product, device/sony/yoshino-common/platform.mk)
 ### PROPRIETARY VENDOR FILES
 $(call inherit-product, vendor/sony/lilac/lilac-vendor.mk)
 
-# Enable updating of APEXes
-$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
-
 ifeq ($(WITH_FDROID),true)
 $(call inherit-product, vendor/fdroid/fdroid-vendor.mk)
 endif
 ifeq ($(WITH_MICROG),true)
 $(call inherit-product, vendor/microg/microg-vendor.mk)
 endif
-
-### APEX
-PRODUCT_COMPRESSED_APEX := false
-
-### DALVIK
-$(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 
 DEVICE_PATH := device/sony/lilac
 
