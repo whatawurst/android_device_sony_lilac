9fa77647f237663babad018ddeea536010e96c71
derfelot <derfelot.git@gmail.com>
2022-08-11 00:24:41 +0200

    lilac: Update all radio related and gnss blobs from SLD_sprout
    Also: * Drop extract-files file modifications
          * Drop now unneeded Sony ril extensions
    Change-Id: Ia7886c27337eed3a3191beb0378d331dbaf472e8

--- extract-files.sh
+++ extract-files.sh
@@ -60,33 +60,4 @@ setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" true "${CLEAN_VENDOR}"
 extract "${MY_DIR}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
 extract "${MY_DIR}/proprietary-files-vendor.txt" "${SRC}" "${KANG}" --section "${SECTION}"
 
-#
-# Fix product path
-#
-
-DEVICE_ROOT="${ANDROID_ROOT}"/vendor/"${VENDOR}"/"${DEVICE}"/proprietary
-
-function fix_product_path () {
-    sed -i \
-        's/\/system\/framework\//\/system\/product\/framework\//g' \
-        "${DEVICE_ROOT}"/"$1"
-}
-
-fix_product_path product/etc/permissions/com.qualcomm.qti.imscmservice-V2.0-java.xml
-fix_product_path product/etc/permissions/com.qualcomm.qti.imscmservice-V2.1-java.xml
-fix_product_path product/etc/permissions/com.qualcomm.qti.imscmservice.xml
-fix_product_path product/etc/permissions/embms.xml
-fix_product_path product/etc/permissions/lpa.xml
-fix_product_path product/etc/permissions/qcrilhook.xml
-fix_product_path product/etc/permissions/telephonyservice.xml
-
-#
-# Add shims to lib-imsvideocodec.so
-#
-
-"${PATCHELF}" --add-needed "libui_shim.so" "${DEVICE_ROOT}"/product/lib/lib-imsvideocodec.so
-"${PATCHELF}" --add-needed "libgui_shim.so" "${DEVICE_ROOT}"/product/lib/lib-imsvideocodec.so
-"${PATCHELF}" --add-needed "libui_shim.so" "${DEVICE_ROOT}"/product/lib64/lib-imsvideocodec.so
-"${PATCHELF}" --add-needed "libgui_shim.so" "${DEVICE_ROOT}"/product/lib64/lib-imsvideocodec.so
-
 "${MY_DIR}"/setup-makefiles.sh
