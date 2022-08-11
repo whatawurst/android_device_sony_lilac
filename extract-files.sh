ca5852e41fc57a7c71df4128ab27b9e37aeeb11f
derfelot <derfelot.git@gmail.com>
2022-08-12 00:50:03 +0200

    lilac: Remove the need for pinned modified stock blobs
    Patch and copy them during extraction instead
    
    Change-Id: Ifcc14a2d81f7420de5437ac1e2545ea681c084f5

--- extract-files.sh
+++ extract-files.sh
@@ -60,4 +60,23 @@ setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" true "${CLEAN_VENDOR}"
 extract "${MY_DIR}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
 extract "${MY_DIR}/proprietary-files-vendor.txt" "${SRC}" "${KANG}" --section "${SECTION}"
 
+#
+# Blobs fixup start
+#
+
+DEVICE_ROOT="${ANDROID_ROOT}"/vendor/"${VENDOR}"/"${DEVICE}"/proprietary
+
+# Fix referenced set_sched_policy for stock audio hal
+"${PATCHELF}" --replace-needed "libcutils.so" "libprocessgroup.so" "${DEVICE_ROOT}"/vendor/lib/hw/audio.primary.msm8998.so
+
+# Let ffu load ufs firmare files from /etc
+sed -i 's/\/lib\/firmware\/ufs/\/etc\/firmware\/ufs/g' "${DEVICE_ROOT}"/vendor/bin/ffu
+
+# Add a restorecon for /persist/wlan to taimport_vendor.rc
+sed -i '4 a\    restorecon /persist/wlan' "${DEVICE_ROOT}"/vendor/etc/init/taimport_vendor.rc
+
+#
+# Blobs fixup end
+#
+
 "${MY_DIR}"/setup-makefiles.sh
