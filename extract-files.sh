ab2d3300169244c1b0e9cc90dd08a20d3da2d3bf
derfelot <derfelot.git@gmail.com>
2022-08-17 23:01:08 +0200

    lilac: Commonize proprietary files for yoshino
    Move the following to yoshino-common:
      * All kanged blobs
      * Those with identical sha1sum across all yoshino devices
      * Use the opportunity to clean up the file lists a bit (alphetical order & separate firmware files from libs)
    
    Change-Id: I7c96c1afa138635ba13975bc47e59f2004b5dd6e

--- extract-files.sh
+++ extract-files.sh
@@ -69,16 +69,6 @@ DEVICE_ROOT="${ANDROID_ROOT}"/vendor/"${VENDOR}"/"${DEVICE}"/proprietary
 # Fix referenced set_sched_policy for stock audio hal
 "${PATCHELF}" --replace-needed "libcutils.so" "libprocessgroup.so" "${DEVICE_ROOT}"/vendor/lib/hw/audio.primary.msm8998.so
 
-# Let ffu load ufs firmare files from /etc
-sed -i 's/\/lib\/firmware\/ufs/\/etc\/firmware\/ufs/g' "${DEVICE_ROOT}"/vendor/bin/ffu
-
-# Add a restorecon for /persist/wlan to taimport_vendor.rc
-sed -i '4 a\    restorecon /persist/wlan' "${DEVICE_ROOT}"/vendor/etc/init/taimport_vendor.rc
-
-# Change xml version from 2.0 to 1.0
-sed -i 's/version\=\"2\.0\"/version\=\"1\.0\"/g' "${DEVICE_ROOT}"/product/etc/permissions/vendor.qti.hardware.data.connection-V1.0-java.xml
-sed -i 's/version\=\"2\.0\"/version\=\"1\.0\"/g' "${DEVICE_ROOT}"/product/etc/permissions/vendor.qti.hardware.data.connection-V1.1-java.xml
-
 #
 # Blobs fixup end
 #
